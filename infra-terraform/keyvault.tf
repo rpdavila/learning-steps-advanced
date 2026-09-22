352f0a285d6e73d9c8bd6e6eecfba6a73d039faa
Rafael Pietri-Davila <rafael.pietri@gmail.com>
2026-09-22 13:57:37 +0200

    added import statement for keyvault with secret identifier

--- infra-terraform/keyvault.tf
+++ infra-terraform/keyvault.tf
@@ -23,6 +23,11 @@ resource "azurerm_role_assignment" "rbac_akv" {
   scope                = azurerm_key_vault.akv.id
 }
 
+import {
+  to = azurerm_key_vault_secret.akv_secret
+  id = "https://learning-steps-key-vault.vault.azure.net/secrets/learning-steps-secret/df0c76b99875449d884a2fac41565557"
+}
+
 resource "azurerm_key_vault_secret" "akv_secret" {
   name         = "learning-steps-secret"
   key_vault_id = azurerm_key_vault.akv.id
